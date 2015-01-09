ef99d7a94a6924d540f2069d3239e441b3895f06
Bogdan Costea <bogdan@costea.us>
2015-01-09 19:33:50 +0200

    install and uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -10,9 +10,7 @@ if [[ $EUID -ne 0 ]]; then
     echo "You must be root to run this script." 2>&1
     exit 1
 else
-    mkdir -p "$INSTALLDIR"
-    cp -r . "$INSTALLDIR/"
-    echo "Installed tomate2 to $INSTALLDIR"
-    ln -s "$INSTALLDIR/tomate2.py" "$BINDIR/tomate2"
-    echo "tomate2 start the app"
+    rm -rf "$INSTALLDIR"
+    rm "$BINDIR/tomate2"
+    echo "uninstalled tomate2"
 fi
